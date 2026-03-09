110e46dabc843a42c4917183411b2ffd502a7e79
Aliyah Lavallée Harrari <aliyah.lavallee.harrari@gmail.com>
2026-03-09 16:33:09 +0100

    refactor: update styles to use Montserrat font and clean up various components

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ export default function Banner({ image, texte }) {
     <div className="banner">
       {image && <img className="banner__img" src={image} alt="Bannière" />}
       <div className="banner__sombre"></div>
-      <h1 className="banner__txt">{texte || "Chez vous, partout et ailleurs"}</h1>
+      <h1 className="banner__txt">{texte}</h1>
     </div>
   );
 }
